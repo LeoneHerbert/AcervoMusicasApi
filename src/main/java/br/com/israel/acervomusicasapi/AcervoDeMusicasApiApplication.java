1bc9db5cab75ba5421941e2dc307986144ae1798
Israel F. Vieira <israelv.generation@gmail.com>
2020-08-22 15:26:55 -0300

    Primeiro commit, inicialização do projeto

---
+++ src/main/java/br/com/israel/acervomusicasapi/AcervoDeMusicasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.israel.acervomusicasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoDeMusicasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervoDeMusicasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/israel/acervomusicasapi/AcervoDeMusicasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.israel.acervomusicasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoDeMusicasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
