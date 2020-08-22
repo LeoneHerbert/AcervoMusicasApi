6be2ce84cdd64d225aade8672fab68e55041a133
Israel F. Vieira <israelv.generation@gmail.com>
2020-08-22 18:33:19 -0300

    refactor(UsuarioModel): Adicionando um construtor ao modelo

--- src/main/java/br/com/israel/acervomusicasapi/models/Usuario.java
+++ src/main/java/br/com/israel/acervomusicasapi/models/Usuario.java
@@ -26,6 +26,12 @@ public class Usuario implements UserDetails {
     )
     private List<Perfil> perfis = new ArrayList<>();
 
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
