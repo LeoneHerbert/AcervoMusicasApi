c6af0a7aa7b1b271c0c961e47f9ff0bf01172856
Israel F. Vieira <israelv.generation@gmail.com>
2020-08-22 17:10:28 -0300

    feature(MusicaRepository): Adicionado o repository para a entidade Musica

---
+++ src/main/java/br/com/israel/acervomusicasapi/repository/MusicaRepository.java
@@ -0,0 +1,13 @@
+package br.com.israel.acervomusicasapi.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.israel.acervomusicasapi.models.Musica;
+
+@Repository
+public interface MusicaRepository extends JpaRepository<Musica, Integer>{
+    Page<Musica> findByNome(String nomeMusica, Pageable paginacao);
+}
