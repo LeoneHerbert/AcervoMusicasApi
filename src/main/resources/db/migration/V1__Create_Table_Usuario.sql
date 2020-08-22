9981f82fbb3b12f635208399ba49247ef715e1a5
Israel F. Vieira <israelv.generation@gmail.com>
2020-08-22 17:45:16 -0300

    fix(Migration: V1): Ajuste no sql, estava faltando uma vírgual após a senha e um NOT NULL

--- src/main/resources/db/migration/V1__Create_Table_Usuario.sql
+++ src/main/resources/db/migration/V1__Create_Table_Usuario.sql
@@ -2,6 +2,6 @@ CREATE TABLE usuario(
     id  INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    senha VARCHAR(255)
+    senha VARCHAR(255) NOT NULL,
     CONSTRAINT pk_id_usuario PRIMARY KEY (id)
 )engine=InnoDB DEFAULT CHARSET=utf8;
