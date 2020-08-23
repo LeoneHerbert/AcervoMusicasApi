b3a628123cbaa9c11f95ad7ff854a759d60482aa
Israel F. Vieira <israelv.generation@gmail.com>
2020-08-22 21:01:22 -0300

    feature(PlaylistModel): Adicionando o modelo de Playlist no projeto, já com seu mapeamento

---
+++ src/main/java/br/com/israel/acervomusicasapi/models/Playlist.java
@@ -0,0 +1,75 @@
+package br.com.israel.acervomusicasapi.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Playlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "playlist_musica",
+        joinColumns = @JoinColumn(name = "playlist_id"),
+        inverseJoinColumns = @JoinColumn(name = "musica_id")
+    )
+    List<Musica> musicas = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Playlist playlist = (Playlist) o;
+        return Objects.equals(id, playlist.id) &&
+                Objects.equals(nome, playlist.nome) &&
+                Objects.equals(musicas, playlist.musicas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, musicas);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public List<Musica> getMusicas() {
+        return Collections.unmodifiableList(musicas);
+    }
+
+    public void adicionarMusica(Musica musica) {
+        this.musicas.add(musica);
+    }
+}
