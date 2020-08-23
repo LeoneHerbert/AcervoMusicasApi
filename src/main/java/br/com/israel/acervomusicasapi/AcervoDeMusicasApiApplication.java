92674419125c2892f17c3fb2a416b87958743bda
Israel F. Vieira <israelv.generation@gmail.com>
2020-08-23 14:38:05 -0300

    feature(Swagger): Adicionado o swagger para documentar a API

--- src/main/java/br/com/israel/acervomusicasapi/AcervoDeMusicasApiApplication.java
+++ src/main/java/br/com/israel/acervomusicasapi/AcervoDeMusicasApiApplication.java
@@ -2,8 +2,10 @@ package br.com.israel.acervomusicasapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AcervoDeMusicasApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/israel/acervomusicasapi/config/ConfiguracaoSeguranca.java
+++ src/main/java/br/com/israel/acervomusicasapi/config/ConfiguracaoSeguranca.java
@@ -56,6 +56,7 @@ public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 
     }
 }
